b1ed209bd1670a8e8615f5390cee6449cf07d5a9
toki <clevertrever31@gmail.com>
2026-02-15 21:49:03 -0600

    Fabric Template added

---
+++ src/client/java/twox/luminnautic/LuminnauticClient.java
@@ -0,0 +1,10 @@
+package twox.luminnautic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LuminnauticClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/twox/luminnautic/LuminnauticDataGenerator.java
@@ -0,0 +1,11 @@
+package twox.luminnautic;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LuminnauticDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
